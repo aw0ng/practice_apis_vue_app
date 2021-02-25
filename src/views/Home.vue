64d31dc74e71fa66d12aea7bd74f548f747afba3
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-02-24 22:35:05 -0500

    add reddit and news api requests to go through rails backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <!-- <div v-for="post in redditPosts" v-bind:key="post.data.name">{{ post.data.title }}</div> -->
+    <div v-for="headline in headlines" v-bind:key="headline.author">{{ headline.title }}</div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      todos: [],
+      redditPosts: [],
+      headlines: [],
+    };
+  },
+  created: function() {
+    // axios.get("https://jsonplaceholder.typicode.com/todos").then(response => {
+    //   console.log(response.data);
+    //   this.todos = response.data;
+    // });
+    axios.get("/api/redditposts").then(response => {
+      console.log(response.data);
+      this.redditPosts = response.data.children;
+    });
+    axios.get("/api/newsheadlines").then(response => {
+      console.log(response.data);
+      this.headlines = response.data.articles;
+    });
+  },
+  methods: {
+    // createTodo: function() {
+    //   var params = {
+    //     completed: this.newCompleted,
+    //     title: this.newTitle,
+    //     userId: this.newUserId,
+    //   };
+    //   axios.post("https://jsonplaceholder.typicode.com/todos", params).then(response => {
+    //     console.log("todos create", response);
+    //     this.todos.push(response.data);
+    //   });
+    // },
   },
 };
 </script>
